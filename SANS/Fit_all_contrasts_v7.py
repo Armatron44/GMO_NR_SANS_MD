050f2785ae0fe9348eccd765d52f1faeb3df0de7
Sandy Armstrong <75567763+Armatron44@users.noreply.github.com>
2022-11-25 16:35:10 +0000

    Add files via upload

---
+++ SANS/Fit_all_contrasts_v7.py
@@ -0,0 +1,81 @@
+import sys
+
+sys.path.append('C:/Users/soo29949/Documents/sasview-main/src')
+
+from bumps.names import *
+
+import sasmodels.core
+from sasmodels.core import load_model
+from sasmodels.bumps_model import Model, Experiment
+from sasmodels.data import load_data, set_beam_stop, set_top
+
+""" IMPORT THE DATA USED """
+load_data_d2 = load_data('d2.txt')
+load_data_d3 = load_data('d3.txt')
+load_data_d4 = load_data('d4.txt')
+
+kernel2 = load_model("rep_ellip_waterGMOsolv_ratio_v3")
+
+model1 = Model(kernel2,
+    scale=0.0033,
+    background=0.0097489,
+    Surf_SLD=0.21,
+    Water_GMO_ratio=0,
+    water_SLD=0,
+    eccentricity=1.88,
+    volume=1.3e5,
+    Solv_vf=0,
+    sld_solvent=6.7
+    )
+    
+model2 = Model(kernel2,
+    scale=0.01,
+    background=0.0097489,
+    Surf_SLD=0.21,
+    Water_GMO_ratio=5,
+    water_SLD=-0.54,
+    eccentricity=1.5,
+    volume=1.3e5,
+    Solv_vf=0,
+    sld_solvent=6.7
+    )
+    
+model3 = Model(kernel2,
+    scale=0.01,
+    background=0.0097489,
+    Surf_SLD=0.21,
+    Water_GMO_ratio=5,
+    water_SLD=6.37,
+    eccentricity=model2.eccentricity,
+    volume=model2.volume,
+    Solv_vf=0,
+    sld_solvent=6.7
+    )
+
+# SET THE FITTING PARAMETERS
+model1.volume.range(0, 1E8)
+model1.volume.name = 'GMO_volume'
+model1.eccentricity.range(0.333, 3)
+model1.eccentricity.name = 'GMO_eccentricity'
+model1.background.range(1E-3, 1)
+model1.background.name = 'GMO_background'
+model1.scale.range(1E-6, 1)
+model1.scale.name = 'GMO_scale'
+model2.background.range(1E-3, 1)
+model2.background.name = 'GMO_H2O_bkg'
+model3.background.range(1E-3, 1)
+model3.background.name = 'GMO_D2O_bkg'
+model2.scale.range(1E-6, 1)
+model2.scale.name = 'GMO_H2O_scale'
+model3.scale.range(1E-6, 1)
+model3.scale.name = 'GMO_D2O_scale'
+model2.eccentricity.range(0.333, 3)
+model2.eccentricity.name = 'GMO_H2O_eccen'
+model2.volume.range(0, 1E8)
+model2.volume.name = 'GMO_H2O_volume'
+
+#set up the fitting objective.
+M1 = Experiment(data=load_data_d2, model=model1)
+M2 = Experiment(data=load_data_d3, model=model2)
+M3 = Experiment(data=load_data_d4, model=model3)
+problem = FitProblem([M1, M2, M3])
